ec631a79cbb26fd328bca6327b1519c1a3325474
Fredylx <84998401+Fredylx@users.noreply.github.com>
2022-08-10 00:08:36 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,76 @@
+# Project made in IOT class
+# Spring 2020
+
+import turtle
+import sys
+
+
+#for i>3:
+
+
+PASS_CODE=qwerty404
+incorrectPassword= True
+while incorrectPassword:
+    password = input("type in your password")
+    if len(password < 8):
+        print("your password must be 8 characters long")
+    elif noNum(password == False):
+        print("you need a number in your password")
+    elif noCap(password == False):
+        print("you need a capital letter in your password")
+    #elif NoLow(password == False
+temp=int(input("Enter temperature:"))
+
+FL= turtle.Pen()
+FL.speed(3)
+FL.shape("arrow")
+
+
+if temp < 25:
+    FL.color("black", "#00ff00") #Green fill
+elif temp >=25 and temp <= 30:
+    FL.color("black", "#8B008B") #Purple fill  
+else:
+    FL.color("black", "#ff0000") #Red fill
+    
+FL.begin_fill()
+FL.forward(50)
+FL.left(90)
+FL.forward(150)
+FL.right(90)
+FL.forward(100)
+FL.left(90)
+FL.forward(50)
+FL.left(90)
+FL.forward(100)
+FL.right(90)
+FL.forward(50)
+FL.right(90)
+FL.forward(150)
+FL.left(90)
+FL.forward(50)
+FL.left(90)
+FL.forward(200)
+FL.left(90)
+FL.forward(300)
+FL.end_fill()
+FL.penup()
+FL.left(90)
+FL.forward(230)
+FL.pendown()
+FL.begin_fill()
+FL.forward(200)
+FL.left(90)
+FL.forward(50)
+FL.left(90)
+FL.forward(150)
+FL.right(90)
+FL.forward(250)
+FL.left(90)
+FL.forward(50)
+FL.left(90)
+FL.forward(300)
+FL.penup()
+FL.end_fill()
+FL.forward(100)
+sys.exit()
