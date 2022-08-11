d1dd9bfb6db1f747f148767542c9a15de5bdaaec
Fredylx <84998401+Fredylx@users.noreply.github.com>
2022-08-11 00:24:17 -0700

    Create itertools-practice.py

---
+++ itertools-practice.py
@@ -0,0 +1,22 @@
+
+import itertools
+
+counter = itertools.count(start=5, step=-2.5)
+# 
+# data = [100, 200, 300, 400]
+# 
+# daily_data = list(zip(itertools.count(), data))
+# 
+# print(daily_data)
+# 
+# 
+
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+
+#for num in counter:
+#    print(num)
+    
+
